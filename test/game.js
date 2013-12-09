eaddf50b90786c775e0f99342927d93d78d8df9f
Jonathan Poulter <poulter7@gmail.com>
2013-12-09 22:27:40 +0000

    Initial project setup

---
+++ test/game.js
@@ -0,0 +1,10 @@
+var assert = require('assert')
+describe('Game', function(){
+	describe('#start', function(){
+		it('should have the standard setup for a backgammon game', function(){
+			[].indexOf(5).should.equal(-1);
+			[].indexOf(5).should.equal(2);
+		})
+	})
+})
+
