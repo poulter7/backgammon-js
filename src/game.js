b9a902a5a55e0c1cae98713b2c39119fdce602fc
Jonathan Poulter <poulter7@gmail.com>
2013-12-21 20:51:51 +0000

    semi-colons for line endings and indentation fixes

--- src/game.js
+++ src/game.js
@@ -28,94 +28,101 @@ function Player(color, board) {
 Player.prototype = {
 	get state() {
 		if (this.color == 'red'){
-			return this.board.redState
+			return this.board.redState;
 		} else {
-			return this.board.blackState
+			return this.board.blackState;
 		}
 	},
 	get opponent(){
-		return this.board[this.color.opponent()]
+		return this.board[this.color.opponent()];
+	},
+	get bar(){
+		if (this.color == 'red'){
+			return this.board.bar.red;
+		} else {
+			return this.board.bar.black;
+		}
 	},
 	canMovePieceAt: function(pos){
-		return this.board.canMovePieceAt(this, pos)
+		return this.board.canMovePieceAt(this, pos);
 	},
 	canMoveToTarget: function(target){
-		return this.board.canMoveToTarget(this, target)
+		return this.board.canMoveToTarget(this, target);
 	},
 	canBearOff: function(pos, roll){
-		return this.board.canBearOff(this, pos, roll)
+		return this.board.canBearOff(this, pos, roll);
 	},
 	placePiece: function(target, roll){
-		return this.board.placePiece(this, target, roll)
+		return this.board.placePiece(this, target, roll);
 	},
 	liftPiece: function(target){
-		return this.board.liftPiece(this, target)
+		return this.board.liftPiece(this, target);
 	},
 	piecesAt: function(target){
-		return this.board.piecesAt(this, target)
+		return this.board.piecesAt(this, target);
 	},
 	validMove: function(pos, roll){
-		return this.board.validMove(this, pos, roll)
+		return this.board.validMove(this, pos, roll);
 	},
 	targetPosition: function (pos, roll){
-		return this.board.targetPosition(this, pos, roll)
+		return this.board.targetPosition(this, pos, roll);
 	},
 	popBar: function(roll){
-		return this.board.popBar(this, roll)
+		return this.board.popBar(this, roll);
 	}
 
 }
 	
 Board.prototype = {
 	get red(){
-		return new Player('red', this)
+		return new Player('red', this);
 	},
 	get black(){
-		return new Player('black', this)
+		return new Player('black', this);
 	},
 	moveRed: function(pos, roll){
-		this.progressPiece(pos, roll)
+		this.progressPiece(pos, roll);
 	},
 	moveBlack: function(pos, roll){
-		this.progressPiece(pos, roll)
+		this.progressPiece(pos, roll);
 	},
 	validMove: function(player, pos, roll){
-		var target = player.targetPosition(pos, roll)
-		var validIfBearOff = !this.wouldBearOff(target) || (player.canBearOff(pos, roll))
+		var target = player.targetPosition(pos, roll);
+		var validIfBearOff = !this.wouldBearOff(target) || (player.canBearOff(pos, roll));
 		return (player.canMoveToTarget(target) && 
 			player.canMovePieceAt(pos) &&
 			validIfBearOff
 		)
 	},
 	popBar: function(player, roll) {
-		target = player.color == 'red' ? roll : 25 - roll
+		target = player.color == 'red' ? roll : 25 - roll;
 		if (player.canMoveToTarget(target)){
 			this.bar[player.color] -= 1;
-			player.placePiece(target, roll)
+			player.placePiece(target, roll);
 		}
 	},
 	toString: function(){
-		return 'Red: ' + JSON.stringify(this.redState) + '\nBlack: ' + JSON.stringify(this.blackState)
+		return 'Red: ' + JSON.stringify(this.redState) + '\nBlack: ' + JSON.stringify(this.blackState);
 	},
 	targetPosition: function(player, pos, roll){
 		if (player.color == 'red'){
-			return pos + roll
+			return pos + roll;
 		} else if (player.color == 'black'){
-			return pos - roll
+			return pos - roll;
 		}
 	},
 	progressPiece: function(pos, roll){
-		var player = undefined
+		var player = undefined;
 		if (this.red.piecesAt(pos) > 0){
-			owner = this.red
+			owner = this.red;
 		} else if (this.black.piecesAt(pos) > 0){
-			owner = this.black
+			owner = this.black;
 		}
 		if (owner){
-			var target = owner.targetPosition(pos, roll)
+			var target = owner.targetPosition(pos, roll);
 			if (owner.validMove(pos, roll)){
-				owner.liftPiece(pos)
-				owner.placePiece(target, roll)
+				owner.liftPiece(pos);
+				owner.placePiece(target, roll);
 			}
 		}
 
@@ -138,56 +145,54 @@ Board.prototype = {
 	placePiece: function(player, target, roll){
 		if (this.wouldBearOff(target)){
 			// bearing off
-			this.home[player.color] += 1
+			this.home[player.color] += 1;
 		} else {
 			if (player.opponent.piecesAt(target) == 1){
-				player.opponent.state[target] = 0
-				this.bar[player.color.opponent()] += 1
+				player.opponent.state[target] = 0;
+				this.bar[player.color.opponent()] += 1;
 			}
 			player.state[target] = player.piecesAt(target) + 1;
 		}
 	},
 	wouldBearOff: function(target){
-		return (target <= 0 || target > 24)
+		return (target <= 0 || target > 24);
 	},
 	canBearOff: function(player, pos, roll){
-		var nonHomeIndices = player.color == 'red'? _.range(1, 19) : _.range(7, 25)
-		var homeIndices = player.color == 'red'? _.range(19, 25) : _.range(1, 7)
-		
-		var nonHomeValues = _.values(_.pick(player.state, nonHomeIndices))
+		var nonHomeIndices = player.color == 'red'? _.range(1, 19) : _.range(7, 25);
+		var homeIndices = player.color == 'red'? _.range(19, 25) : _.range(1, 7);
+		var nonHomeValues = _.values(_.pick(player.state, nonHomeIndices));
+		var homeSubBoard = _.pick(player.state, homeIndices);
 
-		var homeSubBoard = _.pick(player.state, homeIndices)
-
-		var homeValues = []
+		var homeValues = [];
 
 		for (var key in homeSubBoard){
-			var o = homeSubBoard[key]
+			var o = homeSubBoard[key];
 			if (o){
-				homeValues.push(key)
+				homeValues.push(key);
 			}
 
 		}
-		f = player.color == 'red' ? _.min : _.max
-		furthestPip = f(homeValues)
+		f = player.color == 'red' ? _.min : _.max;
+		furthestPip = f(homeValues);
 
 
-		return _.size(nonHomeValues) == 0 && this.bar[player.color] == 0 && furthestPip == pos
+		return _.size(nonHomeValues) == 0 && this.bar[player.color] == 0 && furthestPip == pos;
 			
 	},
 	canMovePieceAt: function(player, pos){
 		if (pos == 'bar'){
-			return true
+			return true;
 		} else {
-			return this.bar[player.color] == 0
+			return this.bar[player.color] == 0;
 		}
 	},
 	canMoveToTarget: function(player, target){
-		return player.opponent.piecesAt(target) < 2
+		return player.opponent.piecesAt(target) < 2;
 	}
 }
 
 var initialBoard = function() {
-	var b = new Board()
+	var b = new Board();
 	b.blackState = {
 		24: 2,
 		13: 5,
@@ -200,7 +205,7 @@ var initialBoard = function() {
 		17: 3,
 		19: 5,
 	}
-	return b
+	return b;
 }
 
 module.exports.Board = Board;
--- test/game.js
+++ test/game.js
@@ -6,236 +6,236 @@ var initialBoard = game.initialBoard
 describe('Game', function(){
 	describe('#start', function(){
 		it('should have the correct starting positions for red', function(){
-			var board = initialBoard()
-			board.red.piecesAt(1).should.equal(2)
-			board.red.piecesAt(12).should.equal(5)
-			board.red.piecesAt(17).should.equal(3)
-			board.red.piecesAt(19).should.equal(5)
+			var board = initialBoard();
+			board.red.piecesAt(1).should.equal(2);
+			board.red.piecesAt(12).should.equal(5);
+			board.red.piecesAt(17).should.equal(3);
+			board.red.piecesAt(19).should.equal(5);
 		}),
 		it('should have the correct starting positions for black', function(){
-			var board = initialBoard()
-			board.black.piecesAt(24).should.equal(2)
-			board.black.piecesAt(13).should.equal(5)
-			board.black.piecesAt(8).should.equal(3)
-			board.black.piecesAt(6).should.equal(5)
+			var board = initialBoard();
+			board.black.piecesAt(24).should.equal(2);
+			board.black.piecesAt(13).should.equal(5);
+			board.black.piecesAt(8).should.equal(3);
+			board.black.piecesAt(6).should.equal(5);
 		})
 	})
 	describe('#move', function(){
 		it('should be able to move ', function(){
-			var board = new Board()
-			board.redState = {1:1}
-			board.red.piecesAt(1).should.equal(1)
-			board.moveRed(1, 1)
-			board.red.piecesAt(1).should.equal(0)
+			var board = new Board();
+			board.redState = {1:1};
+			board.red.piecesAt(1).should.equal(1);
+			board.moveRed(1, 1);
+			board.red.piecesAt(1).should.equal(0);
 			board.red.piecesAt(2).should.equal(1)
 		}),
 		it('should be able to move two points', function(){
-			var board = new Board()
-			board.redState = {1:1}
-			assert.equal(board.red.piecesAt(1), 1)
-			board.moveRed(1, 2)
-			assert.equal(board.red.piecesAt(1), 0)
-			assert.equal(board.red.piecesAt(3), 1)
+			var board = new Board();
+			board.redState = {1:1};
+			assert.equal(board.red.piecesAt(1), 1);
+			board.moveRed(1, 2);
+			assert.equal(board.red.piecesAt(1), 0);
+			assert.equal(board.red.piecesAt(3), 1);
 		}),
 		it('should be able to move splitting a stack', function(){
-			var board = new Board()
-			board.redState = {1:2}
-			board.moveRed(1, 1)
-			board.red.piecesAt(1).should.equal(1)
-			board.red.piecesAt(2).should.equal(1)
+			var board = new Board();
+			board.redState = {1:2};
+			board.moveRed(1, 1);
+			board.red.piecesAt(1).should.equal(1);
+			board.red.piecesAt(2).should.equal(1);
 		}),
 		it('should not be able to move when an opposing stack is blocking', function(){
-			var board = new Board()
-			board.blackState = {2:2}
-			board.redState = {1:1}
-			board.red.validMove(1, 1).should.not.be.okay
-			board.red.canMoveToTarget(2).should.not.be.okay
-			board.moveRed(1, 1)
-			board.red.piecesAt(1).should.equal(1)
-			board.red.piecesAt(2).should.equal(0)
+			var board = new Board();
+			board.blackState = {2:2};
+			board.redState = {1:1};
+			board.red.validMove(1, 1).should.not.be.okay;
+			board.red.canMoveToTarget(2).should.not.be.okay;
+			board.moveRed(1, 1);
+			board.red.piecesAt(1).should.equal(1);
+			board.red.piecesAt(2).should.equal(0);
 			board.black.piecesAt(2).should.equal(2)
 		})
 	})
 	describe('#player', function(){
 		it('should be possible from a player string to retreive the opponent', function(){
-			'red'.opponent().should.equal('black')
-			'black'.opponent().should.equal('red')
-			should.strictEqual('dave'.opponent(), undefined)
+			'red'.opponent().should.equal('black');
+			'black'.opponent().should.equal('red');
+			should.strictEqual('dave'.opponent(), undefined);
 		})
 	})
 	describe('#validMove', function(){
 		it('should be invalid to move into a stack of opposing pieces', function(){
-			var board = new Board()
-			board.blackState = {2:2}
-			board.red.canMoveToTarget(2).should.be.false
+			var board = new Board();
+			board.blackState = {2:2};
+			board.red.canMoveToTarget(2).should.be.false;
 
 		})
 		it('should be invalid to move into a stack of opposing pieces', function(){
-			var board = new Board()
-			board.redState = {2:2}
-			board.black.canMoveToTarget(2).should.be.false
+			var board = new Board();
+			board.redState = {2:2};
+			board.black.canMoveToTarget(2).should.be.false;
 
 		})
 		it('should be valid to move against a single piece', function(){
-			var board = new Board()
-			board.blackState = {2:1}
-			board.red.canMoveToTarget(2).should.be.true
+			var board = new Board();
+			board.blackState = {2:1};
+			board.red.canMoveToTarget(2).should.be.true;
 		})
 		it('should be valid to move into an empty space', function(){
-			var board = new Board()
-			board.blackState = {2:0}
-			board.red.canMoveToTarget(2).should.be.true
+			var board = new Board();
+			board.blackState = {2:0};
+			board.red.canMoveToTarget(2).should.be.true;
 		})
 		it('should be invalid to move anything but bar moves if available', function(){
-			var board = new Board()
-			board.bar = {red:1, black:0}
-			board.red.canMovePieceAt(2).should.be.false
-			board.red.canMovePieceAt('bar').should.be.true
+			var board = new Board();
+			board.bar = {red:1, black:0};
+			board.red.canMovePieceAt(2).should.be.false;
+			board.red.canMovePieceAt('bar').should.be.true;
 		})
 		it('should be valid to move anything if bar is free', function(){
-			var board = new Board()
-			board.redState = {1:1}
-			board.bar = {red:0, black:0}
-			board.red.canMovePieceAt(1).should.be.true
+			var board = new Board();
+			board.redState = {1:1};
+			board.bar = {red:0, black:0};
+			board.red.canMovePieceAt(1).should.be.true;
 		})
 	})
 	describe('#hitting', function(){
 		it('should be possible to hit a blot', function(){
-			var board = new Board()
-			board.blackState = {12:1}
-			board.redState = {11:1}
-			board.moveRed(11, 1)
-			board.red.piecesAt(11).should.equal(0)
+			var board = new Board();
+			board.blackState = {12:1};
+			board.redState = {11:1};
+			board.moveRed(11, 1);
+			board.red.piecesAt(11).should.equal(0);
 			board.bar.black.should.equal(1)
 		})
 		it('should be possible for black to hit a blot', function(){
-			var board = new Board()
-			board.blackState = {12:1}
-			board.redState = {11:1}
-			board.moveBlack(12, 1)
-			board.red.piecesAt(11).should.equal(0)
-			board.black.piecesAt(11).should.equal(1)
-			board.bar.red.should.equal(1)
+			var board = new Board();
+			board.blackState = {12:1};
+			board.redState = {11:1};
+			board.moveBlack(12, 1);
+			board.red.piecesAt(11).should.equal(0);
+			board.black.piecesAt(11).should.equal(1);
+			board.bar.red.should.equal(1);
 		})
 	}),
-	describe('#bar', function(){
-		it('should empty at startup', function() {
-			var board = initialBoard()
-			assert.equal(board.bar.black, 0)
-			board.bar.black.should.equal(0)
-			board.bar.red.should.equal(0)
-		})
-		it('should be able to move a red piece off of the bar to create a stack', function(){
-			var board = new Board()
-			board.redState = {1:1}
-			board.bar = {red:1, black:0}
-			board.red.popBar(1)
-			board.bar.red.should.equal(0)
-			board.red.piecesAt(1).should.equal(2)
-		})
-		it('should be able to move a red piece off of the bar', function(){
-			var board = new Board()
-			board.bar = {red:1, black: 0}
-			board.red.popBar(1)
-			board.bar.red.should.equal(0)
-			board.red.piecesAt(1).should.equal(1)
+		describe('#bar', function(){
+			it('should empty at startup', function() {
+				var board = initialBoard();
+				assert.equal(board.bar.black, 0);
+				board.bar.black.should.equal(0);
+				board.bar.red.should.equal(0);
+			})
+			it('should be able to move a red piece off of the bar to create a stack', function(){
+				var board = new Board();
+				board.redState = {1:1};
+				board.bar = {red:1, black:0};
+				board.red.popBar(1);
+				board.bar.red.should.equal(0);
+				board.red.piecesAt(1).should.equal(2)
+			})
+			it('should be able to move a red piece off of the bar', function(){
+				var board = new Board();
+				board.bar = {red:1, black: 0};
+				board.red.popBar(1);
+				board.bar.red.should.equal(0);
+				board.red.piecesAt(1).should.equal(1);
+			}),
+				it('should be able to move a red piece off of the bar to a different first six point', function(){
+					var board = new Board();
+					board.bar = {red: 1, black: 0};
+					board.red.popBar(6);
+					board.bar.red.should.equal(0);
+					board.red.piecesAt(6).should.equal(1);
+				}),
+				it('should be possible to hit a blot from entering', function(){
+					var board = new Board();
+					board.blackState = {1:1};;
+					board.bar = {red:1, black:0};
+					board.red.popBar(1);
+					board.red.piecesAt(1).should.equal(1, 'Pip successfully entered');
+					board.black.piecesAt(1).should.equal(0, 'Hit pip removed');
+					board.bar.black.should.equal(1, 'Hit pip on bar');
+				}),
+				it('should not be able to pop a piece when a black stack is blocking', function(){
+					var board = new Board();
+					board.blackState = {1:2};
+					board.bar = {red:1, black:0};
+					board.red.popBar(1);
+					board.bar.red.should.equal(1);
+					board.red.piecesAt(1).should.equal(0);
+					board.black.piecesAt(1).should.equal(2);
+				}),
+				it('should be possible to pop a pip from the black bar', function(){
+					var board = new Board();
+					board.redState = {19:1};
+					board.bar = {red:2, black:2};
+					board.black.popBar(6);
+					board.black.piecesAt(19).should.equal(1);
+					board.red.piecesAt(19).should.equal(0);
+					board.bar.red.should.equal(3);
+				})
 		}),
-		it('should be able to move a red piece off of the bar to a different first six point', function(){
-			var board = new Board()
-			board.bar = {red: 1, black: 0}
-			board.red.popBar(6)
-			board.bar.red.should.equal(0)
-			board.red.piecesAt(6).should.equal(1)
-		}),
-		it('should be possible to hit a blot from entering', function(){
-			var board = new Board()
-			board.blackState = {1:1};
-			board.bar = {red:1, black:0}
-			board.red.popBar(1)
-			board.red.piecesAt(1).should.equal(1, 'Pip successfully entered')
-			board.black.piecesAt(1).should.equal(0, 'Hit pip removed')
-			board.bar.black.should.equal(1, 'Hit pip on bar')
-		}),
-		it('should not be able to pop a piece when a black stack is blocking', function(){
-			var board = new Board()
-			board.blackState = {1:2}
-			board.bar = {red:1, black:0}
-			board.red.popBar(1)
-			board.bar.red.should.equal(1)
-			board.red.piecesAt(1).should.equal(0)
-			board.black.piecesAt(1).should.equal(2)
-		}),
-		it('should be possible to pop a pip from the black bar', function(){
-			var board = new Board()
-			board.redState = {19:1}
-			board.bar = {red:2, black:2}
-			board.black.popBar(6)
-			board.black.piecesAt(19).should.equal(1)
-			board.red.piecesAt(19).should.equal(0)
-			board.bar.red.should.equal(3)
-		})
-	}),
-	describe('#bearingoff', function(){
-		it('should be able to bear off black at the end of a game', function() {
-			var board = new Board()
-			board.blackState = {1:1}
-			board.black.canBearOff(1, 1).should.be.ok
-			board.progressPiece(1, 1)
-			board.black.piecesAt(1).should.equal(0)
-			board.home.black.should.equal(1)
-		}),
-		it('should not be able to bear off black with pieces outside the home zone', function() {
-			var board = new Board()
-			board.blackState = {1:1, 8:2}
-			board.black.canBearOff(1, 1).should.not.be.ok
-		}),
-		it('should not be able to bear off black with a larger move to play', function() {
-			var board = new Board()
-			board.blackState = {1:1, 2:2}
-			board.black.canBearOff(1, 2).should.not.be.ok
-			board.black.canBearOff(2, 2).should.be.ok
-		}),
-		it('should not be able to bear off black with a larger move to play, with a large dice roll', function() {
-			var board = new Board()
-			board.blackState = {1:1, 2:2}
-			board.black.canBearOff(1, 6).should.not.be.ok
-			board.black.canBearOff(2, 6).should.be.ok
-		}),
-		it('should be able to bear off red at the end of a game', function() {
-			var board = new Board()
-			board.redState = {24:1}
-			board.red.canBearOff(24, 1).should.be.ok
-			board.progressPiece(24, 1)
-			board.red.piecesAt(24).should.equal(0)
-			board.home.red.should.equal(1)
-		})
-		it('should not be able to bear off if there are pieces outside of the home board', function(){
-			var board = new Board()
-			board.redState = {1: 1, 24:1}
-			board.red.canBearOff(24, 2).should.not.be.ok
-			board.red.validMove(24, 2).should.not.be.ok
+		describe('#bearingoff', function(){
+			it('should be able to bear off black at the end of a game', function() {
+				var board = new Board();
+				board.blackState = {1:1};
+				board.black.canBearOff(1, 1).should.be.ok;
+				board.progressPiece(1, 1);
+				board.black.piecesAt(1).should.equal(0);
+				board.home.black.should.equal(1);
+			}),
+			it('should not be able to bear off black with pieces outside the home zone', function() {
+				var board = new Board();
+				board.blackState = {1:1, 8:2};
+				board.black.canBearOff(1, 1).should.not.be.ok;
+			}),
+			it('should not be able to bear off black with a larger move to play', function() {
+				var board = new Board();
+				board.blackState = {1:1, 2:2};
+				board.black.canBearOff(1, 2).should.not.be.ok;
+				board.black.canBearOff(2, 2).should.be.ok;
+			}),
+			it('should not be able to bear off black with a larger move to play, with a large dice roll', function() {
+				var board = new Board();
+				board.blackState = {1:1, 2:2};
+				board.black.canBearOff(1, 6).should.not.be.ok;
+				board.black.canBearOff(2, 6).should.be.ok;
+			}),
+			it('should be able to bear off red at the end of a game', function() {
+				var board = new Board();
+				board.redState = {24:1};
+				board.red.canBearOff(24, 1).should.be.ok;
+				board.progressPiece(24, 1);
+				board.red.piecesAt(24).should.equal(0);
+				board.home.red.should.equal(1);
+			}),
+			it('should not be able to bear off if there are pieces outside of the home board', function(){
+				var board = new Board();
+				board.redState = {1: 1, 24:1};
+				board.red.canBearOff(24, 2).should.not.be.ok;
+				board.red.validMove(24, 2).should.not.be.ok;
 
+			}),
+			it('should not be able to bear off if there are pieces on the bar', function(){
+				var board = new Board();
+				board.redState = {1: 0, 24:1};
+				board.bar.red = 1;
+				board.red.canBearOff(24, 1).should.not.be.ok;
+				board.progressPiece(24, 1);
+				board.home.red.should.equal(0);
+			}),
+			it('should not be able to bear off a piece when another piece should be moved first', function(){
+				var board = new Board();
+				board.redState = {23:1, 24:1};
+				board.red.validMove(24, 2).should.not.be.ok;
+				board.red.validMove(23, 2).should.be.ok;
+			}),
+			it('should not be able to bear off a piece when another piece could be moved without bearing off', function(){
+				var board = new Board();
+				board.redState = {21:0, 22:1, 24:1};
+				board.red.validMove(24, 2).should.not.be.ok;
+				board.red.validMove(22, 2).should.be.ok;
+			})
 		})
-		it('should not be able to bear off if there are pieces on the bar', function(){
-			var board = new Board()
-			board.redState = {1: 0, 24:1}
-			board.bar.red = 1
-			board.red.canBearOff(24, 1).should.not.be.ok
-			board.progressPiece(24, 1)
-			board.home.red.should.equal(0)
-		})
-		it('should not be able to bear off a piece when another piece should be moved first', function(){
-			var board = new Board()
-			board.redState = {23:1, 24:1}
-			board.red.validMove(24, 2).should.not.be.ok
-			board.red.validMove(23, 2).should.be.ok
-		})
-		it('should not be able to bear off a piece when another piece could be moved without bearing off', function(){
-			var board = new Board()
-			board.redState = {21:0, 22:1, 24:1}
-			board.red.validMove(24, 2).should.not.be.ok
-			board.red.validMove(22, 2).should.be.ok
-		})
-	})
 })
 
