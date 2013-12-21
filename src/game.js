d8a312f346964e16349e057051f2946129483576
Jonathan Poulter <poulter7@gmail.com>
2013-12-21 21:30:16 +0000

    Move player specific functions into the player object

--- src/game.js
+++ src/game.js
@@ -44,31 +44,85 @@ Player.prototype = {
 		}
 	},
 	canMovePieceAt: function(pos){
-		return this.board.canMovePieceAt(this, pos);
+		if (pos == 'bar'){
+			return true;
+		} else {
+			return this.board.bar[this.color] == 0;
+		}
 	},
 	canMoveToTarget: function(target){
-		return this.board.canMoveToTarget(this, target);
+		return this.opponent.piecesAt(target) < 2;
 	},
 	canBearOff: function(pos, roll){
-		return this.board.canBearOff(this, pos, roll);
+		var nonHomeIndices = this.color == 'red'? _.range(1, 19) : _.range(7, 25);
+		var homeIndices = this.color == 'red'? _.range(19, 25) : _.range(1, 7);
+		var nonHomeValues = _.values(_.pick(this.state, nonHomeIndices));
+		var homeSubBoard = _.pick(this.state, homeIndices);
+
+		var homeValues = [];
+
+		for (var key in homeSubBoard){
+			var o = homeSubBoard[key];
+			if (o){
+				homeValues.push(key);
+			}
+
+		}
+		f = this.color == 'red' ? _.min : _.max;
+		furthestPip = f(homeValues);
+
+
+		return _.size(nonHomeValues) == 0 && this.board.bar[this.color] == 0 && furthestPip == pos;
+			
 	},
 	placePiece: function(target, roll){
-		return this.board.placePiece(this, target, roll);
+		if (this.board.wouldBearOff(target)){
+			// bearing off
+			this.board.home[this.color] += 1;
+		} else {
+			if (this.opponent.piecesAt(target) == 1){
+				this.opponent.state[target] = 0;
+				this.board.bar[this.opponent.color] += 1;
+			}
+			this.state[target] = this.piecesAt(target) + 1;
+		}
 	},
-	liftPiece: function(target){
-		return this.board.liftPiece(this, target);
+	liftPiece: function(pos){
+		this.state[pos] -= 1; 
 	},
-	piecesAt: function(target){
-		return this.board.piecesAt(this, target);
+	piecesAt: function(pos){
+		var r = {
+			red: this.board.red.state[pos],
+			black: this.board.black.state[pos],
+		}
+		assert( !this.board.red.state[pos]|| !this.board.black.state[pos], 'cannot have red and black pieces on the same point\n'.concat(this.toString()))
+
+		if (typeof this.state[pos] === "undefined"){
+			this.state[pos] = 0;
+		} 
+		return this.state[pos];
 	},
 	validMove: function(pos, roll){
-		return this.board.validMove(this, pos, roll);
+		var target = this.targetPosition(pos, roll);
+		var validIfBearOff = !this.board.wouldBearOff(target) || (this.canBearOff(pos, roll));
+		return (this.canMoveToTarget(target) && 
+			this.canMovePieceAt(pos) &&
+			validIfBearOff
+		)
 	},
-	targetPosition: function (pos, roll){
-		return this.board.targetPosition(this, pos, roll);
+	targetPosition: function(pos, roll){
+		if (this.color == 'red'){
+			return pos + roll;
+		} else if (this.color == 'black'){
+			return pos - roll;
+		}
 	},
-	popBar: function(roll){
-		return this.board.popBar(this, roll);
+	popBar: function(roll) {
+		target = this.color == 'red' ? roll : 25 - roll;
+		if (this.canMoveToTarget(target)){
+			this.board.bar[this.color] -= 1;
+			this.placePiece(target, roll);
+		}
 	}
 
 }
@@ -86,30 +140,8 @@ Board.prototype = {
 	moveBlack: function(pos, roll){
 		this.progressPiece(pos, roll);
 	},
-	validMove: function(player, pos, roll){
-		var target = player.targetPosition(pos, roll);
-		var validIfBearOff = !this.wouldBearOff(target) || (player.canBearOff(pos, roll));
-		return (player.canMoveToTarget(target) && 
-			player.canMovePieceAt(pos) &&
-			validIfBearOff
-		)
-	},
-	popBar: function(player, roll) {
-		target = player.color == 'red' ? roll : 25 - roll;
-		if (player.canMoveToTarget(target)){
-			this.bar[player.color] -= 1;
-			player.placePiece(target, roll);
-		}
-	},
 	toString: function(){
 		return 'Red: ' + JSON.stringify(this.redState) + '\nBlack: ' + JSON.stringify(this.blackState);
-	},
-	targetPosition: function(player, pos, roll){
-		if (player.color == 'red'){
-			return pos + roll;
-		} else if (player.color == 'black'){
-			return pos - roll;
-		}
 	},
 	progressPiece: function(pos, roll){
 		var player = undefined;
@@ -127,68 +159,9 @@ Board.prototype = {
 		}
 
 	},
-	piecesAt: function(player, pos){
-		var r = {
-			red: this.red.state[pos],
-			black: this.black.state[pos],
-		}
-		assert( !this.red.state[pos]|| !this.black.state[pos], 'cannot have red and black pieces on the same point\n'.concat(this.toString()))
-
-		if (typeof player.state[pos] === "undefined"){
-			player.state[pos] = 0;
-		} 
-		return player.state[pos];
-	},
-	liftPiece: function(player, pos){
-		player.state[pos] -= 1; 
-	},
-	placePiece: function(player, target, roll){
-		if (this.wouldBearOff(target)){
-			// bearing off
-			this.home[player.color] += 1;
-		} else {
-			if (player.opponent.piecesAt(target) == 1){
-				player.opponent.state[target] = 0;
-				this.bar[player.color.opponent()] += 1;
-			}
-			player.state[target] = player.piecesAt(target) + 1;
-		}
-	},
 	wouldBearOff: function(target){
 		return (target <= 0 || target > 24);
 	},
-	canBearOff: function(player, pos, roll){
-		var nonHomeIndices = player.color == 'red'? _.range(1, 19) : _.range(7, 25);
-		var homeIndices = player.color == 'red'? _.range(19, 25) : _.range(1, 7);
-		var nonHomeValues = _.values(_.pick(player.state, nonHomeIndices));
-		var homeSubBoard = _.pick(player.state, homeIndices);
-
-		var homeValues = [];
-
-		for (var key in homeSubBoard){
-			var o = homeSubBoard[key];
-			if (o){
-				homeValues.push(key);
-			}
-
-		}
-		f = player.color == 'red' ? _.min : _.max;
-		furthestPip = f(homeValues);
-
-
-		return _.size(nonHomeValues) == 0 && this.bar[player.color] == 0 && furthestPip == pos;
-			
-	},
-	canMovePieceAt: function(player, pos){
-		if (pos == 'bar'){
-			return true;
-		} else {
-			return this.bar[player.color] == 0;
-		}
-	},
-	canMoveToTarget: function(player, target){
-		return player.opponent.piecesAt(target) < 2;
-	}
 }
 
 var initialBoard = function() {
