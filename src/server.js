2617243788c8d1096fcee661908ee074120eb4af
Jonathan Poulter <jonathan@Jonathans-MacBook-Pro.local>
2013-12-27 19:18:11 +0000

    Adding continuous deployment

--- src/server.js
+++ src/server.js
@@ -5,13 +5,23 @@ currentGame = undefined
 
 start = function(app, port, cb){
 	currentGame = game.initialBoard()
+	currentPlayer = 'red'
 
 	port = process.env.PORT || port || 5000;
 	var server = app.listen(port, cb)
 	io = require('socket.io').listen(server);
 
-	return io.sockets.on('connection', function (socket) {
+	io.sockets.on('connection', function (socket) {
 		socket.on("status", function() {
+			return socket.emit("status", currentGame.state())
+		});
+		socket.on("player", function() {
+			return socket.emit("player", currentPlayer)
+		});
+		socket.on("move", function(pos, roll) {
+			console.log(pos, roll)
+			currentGame.progressPiece(pos, roll)
+			currentPlayer = currentPlayer.opponent();
 			return socket.emit("status", currentGame.state())
 		});
 	});
--- test/server.js
+++ test/server.js
@@ -1,5 +1,6 @@
 var assert = require('assert')
 var should = require('should')
+var _ = require('underscore')
 server = require('../src/server.js')
 
 var should = require('should');
@@ -14,11 +15,9 @@ var options ={
 describe('Game', function(){
 	describe('#view', function(){
 		before(function(done){
-			//server.start(done);
 			var express = require('express');
 			var app = express();
 			server.start(app, 5000, done)
-
 		})
 		after(function(done){
 			server.stop(done);
@@ -26,24 +25,52 @@ describe('Game', function(){
 		it('should be possible to connect to a server', function(done){
 			var client = io.connect(socketURL, options);
 			client.on("connect", function(data){
-				done()
+				done();
 			});
 		})
 		it('should be possible to retreive an initial game state', function(done){
 			var client = io.connect(socketURL, options);
-			var connected = false
-			client.on("connect", function(data){
-				client.emit("status")
-			});
-			game = require('../src/game.js')
+			game = require('../src/game.js');
 
+			client.on("connect", function(data){
+				client.emit("status");
+			});
 			client.on("status", function(data){
+				var positionSummary = _.countBy(data, _.values)
 				assert.deepEqual(
 					data,
 					game.initialBoard().state()
-				)
-				done()
+				);
+				positionSummary['1,red'].should.equal(2);
+				done();
 			});
+		}),
+		it('should be Red to play first', function(done){
+			var client = io.connect(socketURL, options);
+			client.on("connect", function(data){
+				client.emit("player");
+				client.on("player", function(player){
+					player.should.equal('red')
+					done();
+				});
+			});
+		}),
+		it('should be Black to play after a move', function(done){
+			var client = io.connect(socketURL, options);
+			client.on("connect", function(data){
+				client.emit("move", 1, 2)
+				client.on("status", function(data){
+					client.emit("player")
+					var positionSummary = _.countBy(data, _.values);
+					positionSummary['1,red'].should.equal(1);
+					positionSummary['3,red'].should.equal(1);
+					client.on("player", function(player){
+						player.should.equal('black')
+						done();
+					});
+				});
+			});
+				
 		})
 	})
 })
